4dc802ef973b0fd59082e59f8b1dfc1d5d60b619
Feri Lukmansyah <tkjasikk@gmail.com>
2022-02-08 21:48:07 +0700

    Add Email Sercvice for Reset password Development Only

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views 
 
 from . import views
 
@@ -6,6 +7,9 @@ urlpatterns = [
     path("login/", views.loginPage, name="login"),
     path("register/", views.registerPage, name="register"),
     path("logout/", views.logoutUser, name="logout"),
-    path('settings', views.accountSettings, name='accounts_settings')
-    
+    path('settings', views.accountSettings, name='accounts_settings'),
+    path('reset_password/', auth_views.PasswordResetView.as_view(), name='password_reset'),
+    path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('password_confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('reset_password_done/', auth_views.PasswordResetCompleteView.as_view(), name="password_reset_complete"),
 ]
--- core/settings/development.py
+++ core/settings/development.py
@@ -2,17 +2,25 @@ from .base import *
 
 # Installed Apps Settings
 INSTALLED_APPS += [
-    'accounts.apps.AccountsConfig',
-    'dashboard.apps.DashboardConfig',
+    "accounts.apps.AccountsConfig",
+    "dashboard.apps.DashboardConfig",
 ]
 
 DEBUG = True
 
 # Database Settings
 
-DATABASES  = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
+
+# SMTP Configurations
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = "tkjasikk@gmail.com"
+EMAIL_HOST_PASSWORD = "sempakuda"
